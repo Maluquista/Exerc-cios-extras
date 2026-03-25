a5ba809e3f3b1bdfb218ad341eefeaccc50e1b85
Maluquista <gugueliasluiz@gmail.com>
2026-03-24 22:02:24 -0300

    Add files via upload

---
+++ ex07.java
@@ -0,0 +1,19 @@
+package arquivo;
+import java.util.Scanner;
+
+public class ex07 {
+	
+public static void main(String[]args) {
+
+Scanner sc = new Scanner(System.in);
+
+//Elabore um programa que leia 20 valores inteiros, calcule e escreva o percentual de valores negativos e positivos informados.
+
+System.out.println("Informe 20 valores: ");
+
+for()
+
+
+
+}
+}
